3bb92227481243a4e5d7ba2cfe7df5cbcc20c80e
Glen Sale and Leilani Leach <glen@>
2019-01-24 09:46:17 -0800

    set up basic keg constructor and form to add them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/Keg';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  ngOnInit() {
+    document.body.classList.add('backgroundImage');
+  }
+  title = 'Vader\'s Brews';
+  kegList: Keg[] = [
+    new Keg("Jabba's Excellent Ale", "Star Wars Brews", 7.50, 40, 6),
+  ]
+  createKeg(name, brand, pint, keg, abv) {
+    let newKeg = new Keg(name, brand, pint, keg, abv);
+    this.kegList.push(newKeg);
+  }
 }
---
+++ src/app/models/Keg.ts
@@ -0,0 +1,4 @@
+export class Keg {
+
+    constructor(public name: string, public brand: string, public pintPrice: number, public kegPrice: number, public abv: number) { }
+}
