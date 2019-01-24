debed60110f80c7987fa38264dcb0c95eba5b8da
Glen Sale and Leilani Leach <glen@>
2019-01-24 10:24:07 -0800

    add table and button to show/hide form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,15 @@ export class AppComponent {
   title = 'Vader\'s Brews';
   kegList: Keg[] = [
     new Keg("Jabba's Excellent Ale", "Star Wars Brews", 7.50, 40, 6),
+    new Keg("Leia's Pilsner", "Naboo Brews", 6, 30, 4.5),
+    new Keg("Swamp Rat IPA", "Cantina Brewery", 7, 45, 6)
   ]
   createKeg(name, brand, pint, keg, abv) {
     let newKeg = new Keg(name, brand, pint, keg, abv);
     this.kegList.push(newKeg);
+  }
+  showForm: boolean = false;
+  addClicked() {
+    this.showForm = !this.showForm;
   }
 }
--- src/app/models/Keg.ts
+++ src/app/models/Keg.ts
@@ -1,4 +1,24 @@
 export class Keg {
 
+    public capacity: number = 124;
+
+
     constructor(public name: string, public brand: string, public pintPrice: number, public kegPrice: number, public abv: number) { }
+
+    sellBeer(type: string) {
+        if (type == "Pint") {
+            this.capacity--;
+        } else if (type == "Growler") {
+            this.capacity -= 2;
+        } else if (type == "Large") {
+            this.capacity -= 4;
+        }
+    }
+    needReplacement() {
+        if (this.capacity < 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
